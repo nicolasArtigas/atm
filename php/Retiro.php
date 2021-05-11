f7eb25846d55e0ed205bfdd932b3c1daad42a105
NicolasArtigas <nicoartigas83@gmail.com>
2021-05-11 15:14:58 -0300

    Retiro.php con atributos

--- php/Retiro.php
+++ php/Retiro.php
@@ -1,4 +1,9 @@
 <?php
-class Retiro {
+require_once("Teclado.php");
+require_once("DispensadorEfectivo.php");
+class Retiro extends Transaccion {
+	private $monto;
+	private $teclado;
+	private $dispensador;
 
 }
